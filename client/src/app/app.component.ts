de3b30c7d9baab252e6def843d84ad3bb7c9477e
oussamahdidou <oussamahdidou15@gmail.com>
2024-05-16 20:32:06 +0000

    account controller

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as d3 from 'd3';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
-}
+export class AppComponent implements OnInit {
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.createBarChart();
+  }
+
+  private createBarChart(): void {
+    const data = [10, 20, 30, 40, 50];
+
+    const svg = d3.select(this.elementRef.nativeElement).select('.chart')
+      .append('svg')
+      .attr('width', 400)
+      .attr('height', 200);
+
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d, i) => i * 70)
+      .attr('y', (d) => 200 - d)
+      .attr('width', 65)
+      .attr('height', (d) => d)
+      .attr('fill', 'green');
+  }}
