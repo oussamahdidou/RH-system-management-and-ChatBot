0fac9959aef129e54bda5909d686b4b0e9307c19
oussamahdidou <oussamahdidou15@gmail.com>
2024-05-16 00:53:12 +0000

    client side configuration

--- api/helpers/SmtpSettings.cs
+++ api/helpers/SmtpSettings.cs
@@ -7,9 +7,9 @@ namespace api.helpers
 {
     public class SmtpSettings
     {
-        public string Host { get; set; }
+        public string Host { get; set; } = "";
         public int Port { get; set; }
-        public string User { get; set; }
-        public string Pass { get; set; }
+        public string User { get; set; } = "";
+        public string Pass { get; set; } = "";
     }
 }
