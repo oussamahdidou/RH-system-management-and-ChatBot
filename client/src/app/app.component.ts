88d6c6f5219375cdb747c1dfec8c7374d2be38d5
oussamahdidou <oussamahdidou15@gmail.com>
2024-05-30 14:28:18 +0000

    responsive sidebar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   receivedBool: boolean = false;
   /**
    *
    */
-  constructor(public readonly authservice: AuthService) {}
+  showComponent: boolean = true;
+  constructor(
+    public readonly authservice: AuthService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showComponent = !(
+          event.url === '/auth/login' ||
+          event.url === '/' ||
+          event.url === '/AI'
+        );
+      }
+    });
+  }
+
   onMyBoolChange(newBool: boolean) {
     this.receivedBool = newBool;
     console.log('Received boolean value from child:', this.receivedBool);
